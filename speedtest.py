f6bc72aa22dcf4138769e2f889cacbf83e666176
kevin940726 <kevin830726@gmail.com>
2015-01-22 15:35:31 +0800

    Demo Version.

--- speedtest.py
+++ speedtest.py
@@ -7,24 +7,34 @@ from Crypto import Random
 import sys
 
 def hmac_sha224(key, msg):
+	start =  time.clock()
 	hash_obj = hmac.new(key = key, msg = msg, digestmod = hashlib.sha224)
-	return hash_obj.hexdigest()
+	hash_obj.hexdigest()
+	return time.clock()-start
 
 def hmac_sha256(key, msg):
+	start =  time.clock()
 	hash_obj = hmac.new(key = key, msg = msg, digestmod = hashlib.sha256)
-	return hash_obj.hexdigest() 
+	hash_obj.hexdigest() 
+	return time.clock()-start
 
 def hmac_sha384(key, msg):
+	start = time.clock()
 	hash_obj = hmac.new(key = key, msg = msg, digestmod = hashlib.sha384)
-	return hash_obj.hexdigest()
+	hash_obj.hexdigest()
+	return time.clock()-start
 
 def hmac_sha512(key, msg):
+	start = time.clock()
 	hash_obj = hmac.new(key = key, msg = msg, digestmod = hashlib.sha512)
-	return hash_obj.hexdigest()
+	hash_obj.hexdigest()
+	return time.clock()-start
 
 def cmac_aes(key, msg):
+	start = time.clock()
 	cipher = AES.new(key.decode('hex'), AES.MODE_CMAC)
-	return cipher.encrypt(msg).encode('hex')
+	cipher.encrypt(msg).encode('hex')
+	return time.clock()-start
 
 def keyGenerate(bits):
 	return Random.get_random_bytes(bits/8).encode('hex')
@@ -32,29 +42,96 @@ def keyGenerate(bits):
 def msgGenerate(bits):
 	return Random.get_random_bytes(bits/8).encode('hex')
 
-def count(alg, keysize):
-	sum = 0.0
-	for i in range(0, times):
-		key = keyGenerate(keysize)
-		t0 = time.clock()
-		alg(key, msg)
-		sum += time.clock() - t0
-	return times/sum
+def msgRepeat(keySize):	
+	start =  time.clock()
+	fo = open("hmacMsg"+str(keySize), "wb+")
+	fo.write("\tHMAC SHA-224\tHMAC SHA-256\tHMAC SHA-384\tHMAC SHA-512\tCMAC AES-"+str(keySize)+"\n")
 
-times = 100
+	for msgSize in range(8, 1024+8, +8):
+		msg = msgGenerate(msgSize*1024)
+		key = keyGenerate(keySize)
+		items = [msgSize/8, 1/hmac_sha224(key, msg), 1/hmac_sha256(key, msg), 1/hmac_sha384(key, msg), 1/hmac_sha512(key, msg), 1/cmac_aes(key, msg)]
+		for item in items:
+			fo.write("%s\t" %item)
+		fo.write("\n")
+	fo.close()
+	print str(time.clock()-start) + " seconds for total."
+	print "hmacMsg and cmac for key size of " + str(keySize) + " done."
 
-if len(sys.argv) > 1:
-	times = int(sys.argv[1])
+def msgRepeatCmac():
+	start =  time.clock()	
+	fo = open("cmacMsg", "wb+")
+	fo.write("\tCMAC AES-128\tCMAC AES-192\tCMAC AES-256\n")
 
-start = time.clock()	
-fo = open("rawdata", "wb+")
-for msgsize in range(8, 1024+8, +8):
-	msg = msgGenerate(msgsize*1024)
-	items = [msgsize/8, count(hmac_sha224, 224), count(hmac_sha256, 256), count(hmac_sha384, 384), count(hmac_sha512, 512), count(cmac_aes, 128), count(cmac_aes, 192), count(cmac_aes, 256)]
-	for item in items:
-		fo.write("%s\t" %item)
-	fo.write("\n")
-	print msgsize/8
-fo.close()
-print time.clock() - start
+	for msgSize in range(8, 1024+8, +8):
+		msg = msgGenerate(msgSize*1024)
+		key128 = keyGenerate(128)
+		key192 = keyGenerate(192)
+		key256 = keyGenerate(256)
+		items = [msgSize/8, 1/cmac_aes(key128, msg), 1/cmac_aes(key192, msg), 1/cmac_aes(key256, msg)]
+		for item in items:
+			fo.write("%s\t" %item)
+		fo.write("\n")
+	fo.close()
+	print str(time.clock()-start) + " seconds for total."
+	print "cmacMsg done."
 
+def timesRepeat(keySize):
+	start =  time.clock()
+	fo = open("hmac"+str(keySize), "wb+")
+	fo.write("\tHMAC SHA-224\tHMAC SHA-256\tHMAC SHA-384\tHMAC SHA-512\tCMAC AES-"+str(keySize)+"\n")
+
+	for times in range(0, n):
+		msg = msgGenerate(msgsize)
+		key = keyGenerate(keySize)
+		items = [times+1, 1/hmac_sha224(key, msg), 1/hmac_sha256(key, msg), 1/hmac_sha384(key, msg), 1/hmac_sha512(key, msg), 1/cmac_aes(key, msg)]
+		for item in items:
+			fo.write("%s\t" %item)
+		fo.write("\n")
+	fo.close()
+	print str(time.clock()-start) + " seconds for total."
+	print "hmac repeat for "+str(n)+" times with key size of "+str(keySize)+" done."
+
+def timesRepeatCmac():
+	start =  time.clock()
+	fo = open("cmac", "wb+")
+	fo.write("\tCMAC AES-128\tCMAC AES-192\tCMAC AES-256\n")
+
+	for times in range(0, n):	
+		msg = msgGenerate(msgsize)
+		key128 = keyGenerate(128)
+		key192 = keyGenerate(192)
+		key256 = keyGenerate(256)
+		items = [times+1, 1/cmac_aes(key128, msg), 1/cmac_aes(key192, msg), 1/cmac_aes(key256, msg)]
+		for item in items:
+			fo.write("%s\t" %item)
+		fo.write("\n")
+	fo.close()
+	print str(time.clock()-start) + " seconds for total."
+	print "cmac repeat for "+str(n)+" times done."
+
+# Initialize.
+n = 100
+msgsize = 2048
+# Load arguments for parameters.
+if len(sys.argv) == 2:
+	n = int(sys.argv[1])
+elif len(sys.argv) == 3:
+	msgsize = int(sys.argv[2])
+
+
+# Repeat 'n' times for each algorithm.
+timesRepeat(128)
+timesRepeat(192)
+timesRepeat(256)
+timesRepeatCmac()
+# Repeat 8 ~ 1024(*1024bits) times for each message size.
+msgRepeat(128)
+msgRepeat(192)
+msgRepeat(256)
+msgRepeatCmac()
+
+print "For HMAC and CMAC comparing test with each key size for a number of times, check out the output file named 'hmac[keySize]'."
+print "For CMAC comparing with each other for a number of times, check out the output file 'cmac'."
+print "For HMAC and CMAC comparing with each key size for different mwssage size, check out the output file named 'hmacMsg[keySize]'."
+print "For CMAC comparing with each other for different message size, check out the output 'cmacMsg'."
